588f23a500a840fd024396b030214dd51cdb36f2
Numbers analyst <mrgregorybrzeczyszczykiewicz@proton.me>
2025-04-16 16:43:44 +0200

    Refactor code

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
-def ifYearHappy(year: int) -> bool:
+def is_happy_year(year: int) -> bool:
     """Return True if the year has all unique digits."""
-    numbers = [int(x) for x in str(year)]
-    counted_numbers = []
-    for number in numbers:
-        if number in counted_numbers:
-            return False
-        counted_numbers.append(number)
-    return True
+    year_str = str(year)
+    return len(set(year_str)) == len(year_str)
 
 
-def happyYear(year: int) -> int:
+def next_happy_year(year: int) -> int:
     """Return the next happy year after the given year."""
-    year += 1 # start with the next year
-    while not ifYearHappy(year):
+    year += 1  # start with the next year
+    while not is_happy_year(year):
         year += 1
     return year
 
 
 def main():
-    print(happyYear(2017)) # 2018
-    print(happyYear(1990)) # 2013
-    print(happyYear(2021)) # 2031
+    print(next_happy_year(2017))  # 2018
+    print(next_happy_year(1990))  # 2013
+    print(next_happy_year(2021))  # 2031
 
 
 if __name__ == "__main__":
